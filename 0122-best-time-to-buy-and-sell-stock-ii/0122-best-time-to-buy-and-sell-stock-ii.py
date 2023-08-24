1e6223e0942ee420e7f1a495607fed6826382a29
Hyojeong Kim <gywjd5251@gmail.com>
2023-08-25 03:02:58 +0900

    Time: 44 ms (43.78%), Space: 14.8 MB (6.70%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def maxProfit(self, prices):
         
-        # 1. 이익 변수 초기화
+        # 1. prices 배열 요소가 2개 미만일 경우 바로 return 0
+        if len(prices) < 2:
+            return 0
+        
+        # 2. 이익 변수 초기화
         profit = 0
         
         for i in range(1, len(prices)):
-            # 2. 현재 가격이 이전 가격 보다 높을때만 이익이 나므로 이익을 업데이트함
+            # 3. 현재 가격이 이전 가격 보다 높을때만 이익이 나므로 이익을 업데이트함
             if prices[i] > prices[i-1]:
                 profit += prices[i] - prices[i-1]
                 
