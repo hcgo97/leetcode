11d898f18cfa2c2323a8821c192d60008940253b
Hyojeong Kim <gywjd5251@gmail.com>
2023-08-25 03:01:15 +0900

    Time: 41 ms (60.38%), Space: 14.5 MB (44.76%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        
+        # 1. 이익 변수 초기화
+        profit = 0
+        
+        for i in range(1, len(prices)):
+            # 2. 현재 가격이 이전 가격 보다 높을때만 이익이 나므로 이익을 업데이트함
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+                
+        return profit
+                
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
