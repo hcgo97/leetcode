de99cc0790d5202f21618e83753e0e3f4e7a68a9
Hyojeong Kim <gywjd5251@gmail.com>
2023-08-25 01:11:10 +0900

    Time: 347 ms (91.08%), Space: 14.3 MB (82.54%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -9,16 +9,17 @@ class Solution(object):
         move = nums[0]
 
         while move < len(nums):
+            
             # 3. 현재 위치가 배열의 끝에 도달 가능하면 return true
             if move >= len(nums) - 1:
                 return True
 
             # 4-1. 현재 위치에서 더 이동할 수 없는 상황일 때
             if nums[move] == 0:
-
+                
                 # 4-1-1. 이전 위치로 돌아가면서 확인
                 for i in range(move - 1, -1, -1):
-
+                    
                     # 4-1-1-1. 이전 위치에서 현재 위치로 도달 가능한지 확인
                     if i + nums[i] > move:
                         # 4-1-1-2. 진행 가능한 위치로 업데이트
@@ -28,15 +29,15 @@ class Solution(object):
                 # 4-1-2. 더 이상 진행 불가능한 상황이면 return false
                 else:
                     return False
-
+            
             # 4-2. 현재 위치에서 이동할 수 있다면 다시 이동 가능한 범위만큼 이동시킴
             else:
                 move += nums[move]
-
+                
         # 5. 이동 가능한 범위가 배열 요소보다 크면 return true
         if move >= len(nums) - 1:
             return True
-
+                
         # 6. 배열의 마지막까지 도달하지 못한 경우 return false
         return False
                     
