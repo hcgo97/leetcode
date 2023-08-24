7875318b9aa4330639a7b35ed7252f6a8b5814ba
Hyojeong Kim <gywjd5251@gmail.com>
2023-08-24 17:20:38 +0900

    Time: 114 ms (93.81%), Space: 14.8 MB (75.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def majorityElement(self, nums):
         
-        # 1. 배열 정렬
+        # 1. 배열 요소가 1개 밖에 없으면 바로 return
+        if len(nums) == 1:
+            return nums[0]
+        
+        # 2. 배열 정렬
         nums.sort()
         
-        # 2. 정중앙에 위치한 index 구함
+        # 3. 정중앙에 위치한 index 구함
         index = len(nums) // 2
     
-        # 3. 정중앙에 위치한 요소 return
+        # 4. 정중앙에 위치한 요소 return
         return nums[index]
         
         """
