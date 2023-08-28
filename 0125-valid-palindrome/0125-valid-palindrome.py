b7a067dbc71354e3ab80d3b9fd084949f7f975a2
Hyojeong Kim <gywjd5251@gmail.com>
2023-08-28 17:06:50 +0900

    Time: 21 ms (95.13%), Space: 15.4 MB (33.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,16 +4,16 @@ class Solution(object):
     def isPalindrome(self, s):
         
         # 1. 문자 띄워쓰기, 특수문자 없애기
-        s = re.sub(r'[_\W]', '', s)
+        s = re.sub(r'[_\W]', '', s).lower()
         
         # 2. 문자 소문자로 변환
-        s = s.lower()
+        #s = s.lower()
         
         # 3. string 변수 하나 더 만들고 뒤집기
-        sReverse = s[::-1]
+        #sReverse = s[::-1]
         
         # 4. 3 이랑 4 랑 똑같은지 비교
-        if s == sReverse:
+        if s == s[::-1]:
             return True
         else:
             return False
