f2fe8dd9b3b1a0d602b30f2d2798cec1e273f72d
Hyojeong Kim <gywjd5251@gmail.com>
2023-08-28 17:25:12 +0900

    Time: 25 ms (87.48%), Space: 15.3 MB (37.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,10 +6,7 @@ class Solution(object):
         # 1. 문자 특수문자 제거 및 소문자로 변환
         s = re.sub(r'[_\W]', '', s).lower()
         
-        # 2. 변수 하나 더 만들고 뒤집기
-        sReverse = s[::-1]
-        
-        # 3. 원본 문자열과 뒤집힌 원본 문자열이 똑같은지 비교
+        # 2. 원본 문자열과 뒤집힌 원본 문자열이 똑같은지 비교
         if s == s[::-1]:
             return True
         else:
