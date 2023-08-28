552817f40240d844edbd878879cc561bc112def5
Hyojeong Kim <gywjd5251@gmail.com>
2023-08-28 19:36:33 +0900

    Time: 92 ms (71.18%), Space: 14.8 MB (6.83%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def twoSum(self, numbers, target):
-        
+
         # 1. numbers 배열 요소가 2개밖에 없으면 index 1, 2 를 return
         if len(numbers) == 2:
             return [1, 2]
@@ -16,15 +16,15 @@ class Solution(object):
             
             # 4. target 과 left, right 을 더한값을 비교한다.
             if target < sumNumbers:
-                # 4-1. target 보다 크면(<) right 를 -1 해서 포인터 위치를 옮긴다.
+                # 4-1. target 보다 크면(<) right 를 -1 해서 포인터 위치를 옮김
                 right -= 1
 
             elif target > sumNumbers:
-                # 4-2. target 보다 작으면(>) left 를 +1 해서 포인터 위치를 옮긴다.
+                # 4-2. target 보다 작으면(>) left 를 +1 해서 포인터 위치를 옮김
                 left += 1
                 
             else:
-                # 4-3. target 과 같으면 left, right 를 +1 씩해서 return 한다.
+                # 4-3. target 과 같으면 left, right 를 +1 씩해서 return
                 return [left + 1, right + 1]
             
         """
